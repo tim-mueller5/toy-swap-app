518e848be424f398774b277085323776de729ecf
Tim Mueller <trmueller5@gmail.com>
2023-07-17 08:29:19 -0500

    add event details to populate on event cards

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,6 +1,12 @@
 
-function EventCard() {
-    return <p>Event</p>
+function EventCard({ event }) {
+    return (
+        <div>
+            <p>Event Name: {event.name}</p>
+            <p>Location: {event.location}</p>
+            <p>Date: {event.about}</p>
+        </div>
+    )
 }
 
 export default EventCard
